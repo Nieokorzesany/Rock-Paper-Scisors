e33162a71d3d585acdea0adcefa1f0f8a89721a7
Piotr Wlazeł <pancu1222@gmail.com>
2019-06-06 00:42:07 +0200

    add final styling

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ var playerCount = 0;
 var computerCount = 0;
 var roundNumber = 0;
 var message = document.getElementById("message");
-var output = document.getElementById("score");
+var playerscore = document.querySelector(".playerscore");
+var computerscore = document.querySelector(".computerscore");
 var rock = document.getElementById("rock");
 var paper = document.getElementById("paper");
 var scissors = document.getElementById("scissors");
@@ -13,16 +14,30 @@ var play = document.getElementById("play");
 var start = document.querySelector(".start");
 var game = document.querySelector(".game");
 var whowins = document.querySelector(".whowins");
+var resetBox = document.getElementById("reset-div");
 
 play.addEventListener("click", function() {
   var rounds = prompt("Please enter the number of rounds");
   var roundsInt = parseInt(rounds);
   roundNumber = roundsInt;
+  numberCheck();
   start.style.display = "none";
+  game.style.display = "block";
+  resetBox.style.display = "none";
 });
 
-output.innerHTML =
-  "Player score: " + playerCount + " Computer score: " + computerCount;
+function numberCheck() {
+  while (true) {
+    if (roundNumber <= 1) {
+      rounds = prompt("Please enter positive number bigger than 1");
+      roundsInt = parseInt(rounds);
+      roundNumber = roundsInt;
+    } else return;
+  }
+}
+
+playerscore.innerHTML = playerCount;
+computerscore.innerHTML = computerCount;
 
 rock.addEventListener("click", function() {
   playerMove(1);
@@ -35,30 +50,40 @@ scissors.addEventListener("click", function() {
 });
 reset.addEventListener("click", function() {
   playerCount = 0;
+  playerscore.innerHTML = playerCount;
   computerCount = 0;
-  output.innerHTML =
-    "Player score: " + playerCount + " Computer score: " + computerCount;
-  message.innerHTML = "";
+  computerscore.innerHTML = computerCount;
+  start.style.display = "flex";
+  resetBox.style.display = "none";
+  game.style.display = "none";
 });
 
 //rock=1, paper=2, scissors=3
 function whoWinsRound() {
   if (playerPick == computerPick) {
-    message.innerHTML = "tie";
+    message.innerHTML = "Tie";
   } else if ((playerPick - computerPick + 3) % 3 == 1) {
     playerCount++;
-    message.innerHTML = "Player wins";
+    playerscore.innerHTML = playerCount;
+    message.innerHTML = "Player wins round";
   } else {
     computerCount++;
-    message.innerHTML = "Computer wins";
+    computerscore.innerHTML = computerCount;
+    message.innerHTML = "Computer wins round";
   }
 }
 
 function whoWinsGame() {
   if (playerCount >= roundNumber) {
-    alert("Player wins the game");
+    game.style.display = "none";
+    resetBox.style.display = "block";
+    start.style.display = "none";
+    whowins.innerHTML = "Player wins";
   } else if (computerCount >= roundNumber) {
-    alert("computer wins the game");
+    game.style.display = "none";
+    resetBox.style.display = "block";
+    start.style.display = "none";
+    whowins.innerHTML = "Computer wins";
   }
 }
 
@@ -66,7 +91,5 @@ function playerMove(move) {
   playerPick = move;
   computerPick = Math.floor(Math.random() * 3 + 1);
   whoWinsRound();
-  output.innerHTML =
-    "Player score: " + playerCount + " Computer score: " + computerCount;
   whoWinsGame();
 }
